962f6fa058ff52558f47a84e702535120d1568e7
Ausaf Ahmed <ausaf.ahmed@hotmail.com>
2019-08-10 00:07:25 -0700

    Train route to train and save model

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, Response
+from flask import Flask, Response, request, jsonify
+from sklearn import svm
+from sklearn import datasets
+from sklearn.externals import joblib
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/train', methods=['POST'])
+def train():
+    # get parameters from request
+    parameters =  request.get_json()
+
+    # read iris dataset
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
+
+    # fit model
+    clf = svm.SVC(C=float(parameters['C']),
+                  probability=True,
+                  random_state=1)
+    clf.fit(X, y)
+
+    # persist model
+    joblib.dump(clf, 'model.pkl')
+
+    return  jsonify({'accuracy' : round(clf.score(X, y) * 100, 2)})
+
 
 @app.route('/health')
 def health_check():
